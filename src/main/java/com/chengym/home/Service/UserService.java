0803cc2adc4a2acaddd173ca90b831cd28b4b188
Yeoman <yongmingch@126.com>
2018-03-22 17:26:52 +0800

    /////firsttime  add

---
+++ src/main/java/com/chengym/home/Service/UserService.java
@@ -0,0 +1,15 @@
+package com.chengym.home.Service;
+
+import com.chengym.home.Bean.User;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/8/16.
+ */
+public interface UserService {
+
+    int addUser(User user);
+
+    List<User> findAllUser(int pageNum, int pageSize);
+}
